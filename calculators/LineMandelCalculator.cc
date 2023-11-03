861a6f38fae0e578b757d6446634878c2f1a757f
xkrato61 <xkrato61@login1.barbora.it4i.cz>
2023-11-03 09:16:41 +0100

    small opt

--- calculators/LineMandelCalculator.cc
+++ calculators/LineMandelCalculator.cc
@@ -35,32 +35,41 @@ LineMandelCalculator::~LineMandelCalculator() {
 }
 
 int *LineMandelCalculator::calculateMandelbrot() {
-    int *pdata = data;
-	float *xs = xvals, *ys = yvals, *xc = xcalc, xb, r2, i2;
+    int *pdata = data, r;
+	float *xs = xvals, *ys = yvals, *xc = xcalc, xb, r2, i2, y;
 
+	
     for (int i = 0; i < height/2; i++) {
-		const float y = y_start + i * dy;
-		const int r = i*width;
-
-		#pragma omp simd aligned(xs, ys, xc, pdata: 64) simdlen(16)
+		y = y_start + i * dy;
+		r = i*width;
+		#pragma omp simd aligned(xs, ys, xc, pdata: 64)
 		for (int j = 0; j < width; j++) {
 			pdata[r + j] = 0;
-			xb = x_start + j * dx;
+			xb = x_start + (j * dx);
 			xs[j] = xb;
 			ys[j] = y;
 			xc[j] = xb;
 		}
-		
+		int first = 0;
+		bool f;
 		for (int k = 0, escaped=0; k < limit && escaped < width; ++k) {
 			escaped = 0;
-			#pragma omp simd aligned(xs, ys, xc, pdata:64) reduction(+:escaped) simdlen(16)
+			f = true;
+			#pragma omp simd aligned(xs, ys, xc, pdata:64) simdlen(16)
 			for (int j = 0; j < width; j++) {
+
 				r2 = xs[j] * xs[j];
 				i2 = ys[j] * ys[j];
 
-				(r2 + i2 < 4.0f) ? 
-					pdata[r + j]++ : 
+				if ((r2 + i2 < 4.0f)) {
+					pdata[r + j]++;
+					if (f && first <= j) {
+						f = false;
+						first = j;
+					}
+				} else {
 					escaped++;
+				}
 				
 				ys[j] = 2.0f * xs[j] * ys[j] + y;
 				xs[j] = r2 - i2 + xc[j];
